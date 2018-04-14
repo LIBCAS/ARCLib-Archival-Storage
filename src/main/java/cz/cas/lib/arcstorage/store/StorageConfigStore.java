bbdcb8ec5afa4132dc075ca501fb48ec9585f889
Jan Tomášek <yan.tomasek@gmail.com>
2018-04-14 13:49:28 +0200

    randomly choose storage for retrieval if there are more with same priority

--- src/main/java/cz/cas/lib/arcstorage/store/StorageConfigStore.java
+++ src/main/java/cz/cas/lib/arcstorage/store/StorageConfigStore.java
@@ -3,8 +3,12 @@ package cz.cas.lib.arcstorage.store;
 import com.querydsl.jpa.JPAExpressions;
 import cz.cas.lib.arcstorage.domain.QStorageConfig;
 import cz.cas.lib.arcstorage.domain.StorageConfig;
+import cz.cas.lib.arcstorage.exception.GeneralException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Random;
 
 @Repository
 @Slf4j
@@ -17,8 +21,10 @@ public class StorageConfigStore extends DomainStore<StorageConfig, QStorageConfi
     public StorageConfig getByPriority() {
         QStorageConfig qConfig = qObject();
         QStorageConfig qConfig2 = new QStorageConfig("nested");
-        StorageConfig config;
-        config = query().select(qConfig).where(qConfig.priority.eq(JPAExpressions.select(qConfig2.priority.max()).from(qConfig2))).fetchOne();
+        List<StorageConfig> configs = query().select(qConfig).where(qConfig.priority.eq(JPAExpressions.select(qConfig2.priority.max()).from(qConfig2))).fetch();
+        if (configs.isEmpty())
+            throw new GeneralException("no active storage attached");
+        StorageConfig config = configs.get(new Random().nextInt(configs.size()));
         detachAll();
         return config;
     }
